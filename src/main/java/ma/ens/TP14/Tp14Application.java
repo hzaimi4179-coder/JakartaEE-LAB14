97342932abf0ebdd2e4df7c0d86402cabb2e3eee
hzaimi4179-coder <h.zaimi4179@uca.ac.ma>
2025-11-28 16:09:55 +0100

    Add files via upload

---
+++ src/main/java/ma/ens/TP14/Tp14Application.java
@@ -0,0 +1,13 @@
+package ma.ens.TP14;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tp14Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tp14Application.class, args);
+	}
+
+}
---
+++ src/test/java/ma/ens/TP14/Tp14ApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.ens.TP14;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tp14ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
